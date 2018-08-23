2ed91e9d30bcf33332de2b41b96d20fe9b32c055
yangxin <yangxin@tzpt.com>
2018-08-23 17:47:57 +0800

    新增状态栏跟随播放器一起变化.新增网络提示框。本地视频播放换了一种方式。

---
+++ Example/NicooPlayer/View/VideoPlayerFatherView.swift
@@ -0,0 +1,21 @@
+//
+//  VideoPlayerFatherView.swift
+//  NicooPlayer_Example
+//
+//  Created by 小星星 on 2018/8/23.
+//  Copyright © 2018年 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class VideoPlayerFatherView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ NicooNetWorkAlert.swift
@@ -0,0 +1,20 @@
+//
+//  NicooNetWorkAlert.swift
+//  NicooPlayer
+//
+//  Created by 小星星 on 2018/8/23.
+//
+
+import UIKit
+
+class NicooNetWorkAlert: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
